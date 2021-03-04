aa4d738c40f383c820acc6ee1ae87eb57ad6d55e
Hans Von <hans.php@gmail.com>
2021-03-04 16:30:19 -0600

    Listas

--- 01_basico/02_datatypes.dart
+++ 01_basico/02_datatypes.dart
@@ -27,4 +27,26 @@ main() {
 
   // print(activo);
 
+  // ====================== Listas o arreglos
+  // List<String> personajes = ['Superman', 'Batman'];
+  List<String> personajes = new List(); // Lista dinámica.
+  personajes.add('Superman');
+  personajes.add('Batman');
+
+  personajes.addAll(['Supergirl', 'Flash']);
+
+  personajes
+    ..add('Arrow')
+    ..add('Canario negro')
+    ..add('Flash'); // Toleracia a duplicados.
+
+  // print(personajes);
+
+  List<String> villanos = new List(3); // Lista de ancho fijo.
+  villanos[0] = 'Lex';
+  villanos[1] = 'Zoom';
+  villanos[2] = 'Sicada';
+
+  // print(villanos);
+
 }
