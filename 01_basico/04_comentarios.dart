76993ccfedc150a18975e0f519c2e2bcbb53850c
Hans Von <hans.php@gmail.com>
2021-03-08 19:44:46 -0600

    Comentarios

---
+++ 01_basico/04_comentarios.dart
@@ -0,0 +1,23 @@
+/**
+ * Este archivo tiene por objetivo los comentarios en dart.
+ * 
+ * [creadoEn] : 8 de marzo de 2021.
+*/
+
+main() {
+  // Esta es una variable temporal que quiero definir.
+  int a = 10;
+
+/*
+  final personas = [
+    'Fernando', // [0]
+    'Pedro', // [1]
+    'Ingrid', // [2]
+    'Carlos' // [3]
+  ];
+  */
+}
+
+/// Función para saludar.
+/// Recibe un [nombre] y concatena con el [mensaje],
+saludar(int nombre, int mensaje) {}
