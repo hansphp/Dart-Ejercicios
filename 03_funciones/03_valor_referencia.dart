81e52ef2e2e18ca7b38e21d628545f46933e154c
Hans Von <hans.php@gmail.com>
2021-03-09 14:38:31 -0600

    Argumentos por valor y referencia

---
+++ 03_funciones/03_valor_referencia.dart
@@ -0,0 +1,27 @@
+main() {
+  String nombre = 'fernando';
+  String nombre2 = capitalizar(nombre);
+
+  // print(nombre);
+  // print(nombre2);
+
+  Map<String, String> personita = {'nombre': 'juan carlos'};
+
+  Map<String, String> personita2 = capitalizarMapa(personita);
+
+  print(personita);
+  print(personita2);
+}
+
+// booleanos, string, numeros
+String capitalizar(String texto) {
+  texto = texto.toUpperCase();
+  return texto.toUpperCase();
+}
+
+Map<String, String> capitalizarMapa(Map<String, String> persona) {
+  persona = {...persona}; // Operador spread, para clonar objeto.
+
+  persona['nombre'] = persona['nombre'].toUpperCase();
+  return persona;
+}
